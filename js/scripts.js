132ba5d72d3fc473e997ab7e0ac00047e4bcbf05
Mohamed-Omarrr <mm.mohamed.mo007@gmail.com>
2020-07-27 17:45:00 +0300

    added contact form along with information for the what we do section

---
+++ js/scripts.js
@@ -0,0 +1,96 @@
+function form(){
+
+    alert("We thank you for your feedback")
+}
+
+
+
+function design(){
+
+
+   
+
+
+    const img3 = document.getElementById('img3');
+const myText3 = document.getElementById('text3');
+
+img3.addEventListener('click', function(){
+  const myInsertText = 'Our design practice offers a full range of services including brand strategy, interaction and visual design along with user testing experience.';
+myText3.innerHTML = myInsertText;
+
+
+var x = document.getElementById("img3");
+if (x.style.display === "none") {
+  x.style.display = "block";
+} else {
+  x.style.display = "none";
+}
+
+
+
+
+
+});
+}
+
+
+
+
+function development(){
+
+
+   
+
+
+    const img4 = document.getElementById('img4');
+const myText4 = document.getElementById('text4');
+
+img4.addEventListener('click', function(){
+  const myInsertText = 'All engineers are fluent in the latest enterprise, mobile and web development technologies. They collaborate with your team to write, and improve code on a daily basis, using proven practises such as test-driven development and pair programming.';
+myText4.innerHTML = myInsertText;
+
+
+var x = document.getElementById("img4");
+if (x.style.display === "none") {
+  x.style.display = "block";
+} else {
+  x.style.display = "none";
+}
+
+
+
+
+
+});
+}
+
+
+
+
+function product(){
+
+
+   
+
+
+    const img5 = document.getElementById('img5');
+const myText5 = document.getElementById('text5');
+
+img5.addEventListener('click', function(){
+  const myInsertText = 'Planning and development is iterative, because we are constantly coding and testing the products we build. These products are always ready to go live. This iterative process allows for change as business requirements evolve.';
+myText5.innerHTML = myInsertText;
+
+
+var x = document.getElementById("img5");
+if (x.style.display === "none") {
+  x.style.display = "block";
+} else {
+  x.style.display = "none";
+}
+
+
+
+
+
+});
+}
